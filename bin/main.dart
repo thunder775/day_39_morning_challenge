3a71e8b1057d5783b3cab845f85e045f643b9a76
McLarenCollege <hello@mclarencollege.in>
2019-11-27 08:31:04 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,8 @@
+// Tower of Hanoi
+// https://www.ocf.berkeley.edu/~shidi/cs61a/w/images/f/f8/Hanoi0.png
+// Find possible moves for the given state of Hanoi towers
+// input: [[3,4],[1,2],[]]
+// output: [[[1,3,4],[2],[]], [[3,4],[2],[1]], [[4],[1,2],[3]]]
+
+main() {
+}
